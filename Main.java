9ade8e0f10aeee634f1827719714c3c4b20d316a
Serenella9220 <118180225+Serenella9220@users.noreply.github.com>
2024-03-06 09:58:52 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Rectangle rectangle = new Rectangle(5.0, 3.0);
+
+        
+        System.out.println("Area of the Rectangle: " + rectangle.calculateArea());
+
+        
+        Triangle triangle = new Triangle(4.0, 6.0);
+
+        
+        System.out.println("Area of the Triangle: " + triangle.calculateArea());
+    }
+
+   
+}
+
+
+
+
+
